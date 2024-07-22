09136c75d63e68c35ba70a4a61b1de22477a73e0
Ranjan@432221 <ranjankumarsahoo828@gmail.com>
2024-07-22 16:22:23 +0530

    it is a js a html only

---
+++ first.js
@@ -0,0 +1,84 @@
+//console.log(fullname);
+//fullname = "tony starck";
+//age = 24;
+//console.log(age);
+//x = null;
+//y = undefined;
+//console.log(undefined);
+//console.log(x);
+//isfollow = false;
+//console.log(isfollow);
+//js is dynamiccally typed language
+//fullname = 25;
+//console.log(fullname);
+//fullname = "ranjan kumar sahoo";
+//FULLNAME = "chandan kumar sahoo";
+//console.log(fullname);
+//console.log(FULLNAME);
+//fullName--->camel case it is a type of write a variable:
+//isfollow:
+//let fullName = "tony starck";
+//console.log(fullName);
+//{
+//    let a = 25;
+  //  console.log(a);
+//}
+//primitive 7 number,string,boolean,undefind,null,bigint,symbol---->
+//let age =24;
+//let fullName="tony starck";
+//isFollow=true;
+//let x = BigInt("123");
+//non-primitive-->objects -->it is a collection of value
+//object--->collection of values
+//object=>collection of different variable=>key:value
+//const student ={
+  //fullName:"ranjan kumar sahoo",
+  //age:20,cgpa:8.2,
+  //isPass:true,
+//};
+//student["age"] = student["age"]+1;
+//console.log(student.age);
+ //student["fullName"] = "rahul sharma";
+ //console.log(student.fullName);
+ //student["cgpa"] = 9.5;
+ //console.log(student.cgpa);
+ //comment single line and double line comment
+//arithmetic operator
+//let a= 5;//number
+//let b= "5";//string-->number
+//console.log("a=",a,"&b=",b);
+//console.log("a+b=",a+b);
+//console.log("a-b=",a-b);
+//console.log("a*b=",a*b);
+//console.log("a/b=",a/b);
+//console.log("a%b=",a%b);
+//console.log("a**b=",a**b);//5^2
+//unary operator
+//a++;//6
+//console.log("a=",a);
+//a--;
+//console.log("a--=",a);
+//assignment operator
+//console.log(a+=3);
+//console.log(a==b);//false
+//console.log(a!=b);
+//let a= 5;//number
+//let b= "5";//string-->number
+//console.log("a=",a,"&b=",b);
+//console.log(a==b);//true
+//strict equal to and strict not equal to (===) and (!==);
+//console.log(a===b);//false
+//console.log(a!==b);//true
+//LOGICAL OPERATOR &&,||,!;
+//CONDITIONAL STATEMENT;
+let mode="dark";
+let color;
+if(mode==="dark")
+{
+  color="black";
+}
+if(mode==="light")
+{
+  color="white";
+}
+console.log(color);
