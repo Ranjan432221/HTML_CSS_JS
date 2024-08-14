5f2a70a8f15a8a773dacd4a732f282a0fa173ab9
Ranjan@432221 <ranjankumarsahoo828@gmail.com>
2024-08-14 14:13:04 +0530

    it is a  some learning js

---
+++ second.js
@@ -0,0 +1,53 @@
+// let color;
+// let mode = "light";
+// if(mode==="dark-mode")
+// {
+//     color = "black";
+// }
+// if(mode==="light")
+// {
+//     color = "white";
+// }
+// console.log(color);
+// let age = 16;
+// if (age >= 18)
+// {
+//     console.log("do your vote");
+// }
+// else
+// {
+//     console.log("dont do your vote")
+// }
+// let num = 10;
+// if(num % 2=== 0)
+// {
+//     console.log("it is even");
+// }
+// else
+// {
+//     console.log("it is odd");
+// }
+//ELSE-IF STATEMENT
+let mode = "dark";
+let color;
+if(mode==="dark")
+{
+    color = "black";
+}
+else if(mode === "blue")
+{
+    color = "blue";
+}
+else if(mode=== "pink")
+{
+    color = "pink";
+}
+else
+{
+    color = "white";
+}
+console.log(color);
+if(mode === "dark") 
+{
+    console.log(mode);
+}
