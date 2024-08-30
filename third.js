489d310a9ddb17637bdc4374babc1b14ee502fe1
Ranjan@432221 <ranjankumarsahoo828@gmail.com>
2024-08-30 19:37:57 +0530

    dome practice code of the js

---
+++ third.js
@@ -0,0 +1,17 @@
+//ternary operator about some thing
+//a?b:c;
+// let age = 25;
+// let result = (age > 18?"adult":"not adult");//simpler,compact-if else statement
+// console.log(result);
+//MDN DOCS
+//alert("hello!")//one time popup
+let number = prompt("enter any number");
+
+if(number % 5 === 0)
+{
+    console.log( number,"it is multiple of 5");
+}
+else
+{
+    console.log(number,"it is not multiple of 5");
+}
